8952c8a6ccb4e93615631e41b5415a7aa2847177
OllisDev <ikermagro@outlook.es>
2026-03-03 19:02:11 +0100

    model and migration Valoracion

---
+++ src/app/Models/Valoracion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Valoracion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'valoracion';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_tutoria',
+        'puntuacion',
+        'comentario',
+        'fecha'
+    ];
+}
