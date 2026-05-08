f570b634ccd37691e2eebbb0476147253d56ee64
OllisDev <ikermagro@outlook.es>
2026-05-08 16:37:28 +0200

    fix vite compilation

--- src/vite.config.js
+++ src/vite.config.js
@@ -6,7 +6,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            input: [
+                "resources/css/app.css",
+                "resources/css/login.css",
+                "resources/css/register.css",
+                "resources/css/feed.css",
+                "resources/css/offers.css",
+                "resources/css/myRequests.css",
+                "resources/css/assignment.css",
+                "resources/css/tutorial.css",
+                "resources/js/app.jsx",
+                "resources/js/registerApp.jsx",
+            ],
             refresh: true,
         }),
         tailwindcss(),
